2bfb9824dc988463882cdba576b839cd0e1efd3a
AdinGRT <adingrt@gmail.com>
2021-01-30 14:31:26 -0600

    se testeo el metodo listar en la Clase TestUsuario

--- src/main/java/test/TestUsuario.java
+++ src/main/java/test/TestUsuario.java
@@ -12,6 +12,9 @@ public class TestUsuario {
     public static void main(String[] args) {
         var usuarioDao = new UsuarioDAO();
         
+        var usuarioNuevo = new Usuario("atrejo", "555bbb");
+        usuarioDao.insertar(usuarioNuevo);
+        
         List<Usuario> usuarios = usuarioDao.listar();
         usuarios.forEach(usuario -> {
             System.out.println("usuario = " + usuario);
