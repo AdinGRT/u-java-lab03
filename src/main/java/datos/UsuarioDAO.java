b1a341f92f73db2e24ce24f229493a8c1b519849
AdinGRT <adingrt@gmail.com>
2021-01-30 13:36:59 -0600

    se creo la Clase UsuarioDAO con las cadenas de querys a realizar

---
+++ src/main/java/datos/UsuarioDAO.java
@@ -0,0 +1,15 @@
+package datos;
+
+
+
+/**
+ *
+ * @author adingrt
+ */
+public class UsuarioDAO {
+    private static final String SQL_SELECT = "SELECT id_usuario, usuario, password FROM usuario";
+    private static final String SQL_INSERT = "INSERT INTO usuario (usuario, password) VALUES (?, ?)";
+    private static final String SQL_UPDATE = "UPDATE usuario SET usuario = ?, password = ? WHERE id_usuario = ?";
+    private static final String SQL_DELETE = "DELETE FROM usuario WHERE id_usuario = ?";
+    
+}
