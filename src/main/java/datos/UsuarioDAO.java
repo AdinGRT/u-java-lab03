742b63712b8d053d93c420a33c8c0d6233f2427e
AdinGRT <adingrt@gmail.com>
2021-01-30 14:17:09 -0600

    se agrego el metodo listar() a la clase UsuarioDAO

--- src/main/java/datos/UsuarioDAO.java
+++ src/main/java/datos/UsuarioDAO.java
@@ -1,6 +1,8 @@
 package datos;
 
-
+import domain.Usuario;
+import java.sql.*;
+import java.util.*;
 
 /**
  *
@@ -11,5 +13,40 @@ public class UsuarioDAO {
     private static final String SQL_INSERT = "INSERT INTO usuario (usuario, password) VALUES (?, ?)";
     private static final String SQL_UPDATE = "UPDATE usuario SET usuario = ?, password = ? WHERE id_usuario = ?";
     private static final String SQL_DELETE = "DELETE FROM usuario WHERE id_usuario = ?";
+    
+    public List<Usuario> listar() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Usuario usuario = null;
+        List<Usuario> usuarios = new ArrayList<>();
+        
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                int id_usuario = rs.getInt("id_usuario");
+                String username = rs.getString("usuario");
+                String password = rs.getString("password");
+                usuario = new Usuario(id_usuario, username, password);
+                usuarios.add(usuario);
+            }   
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            try {
+                Conexion.close(conn);
+                Conexion.close(stmt);
+                Conexion.close(rs);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+        
+        
+        return usuarios;
+    }
+    
     
 }
