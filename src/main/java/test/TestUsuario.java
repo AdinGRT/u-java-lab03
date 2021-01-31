6196d822a10d09a3bb48273c4f67ec4222f45f1d
AdinGRT <adingrt@gmail.com>
2021-01-30 19:44:54 -0600

    se testeo el metodo eliminar() de la Clase UsuarioDAO

--- src/main/java/test/TestUsuario.java
+++ src/main/java/test/TestUsuario.java
@@ -15,8 +15,11 @@ public class TestUsuario {
         //var usuarioNuevo = new Usuario("atrejo", "555bbb");
         //usuarioDao.insertar(usuarioNuevo);
         
-        var usuarioMod = new Usuario(3, "gtrejo", "123ABC");
-        usuarioDao.actualizar(usuarioMod);
+        //var usuarioMod = new Usuario(3, "gtrejo", "123ABC");
+        //usuarioDao.actualizar(usuarioMod);
+        
+        var usuarioEliminar = new Usuario(3);
+        usuarioDao.eliminar(usuarioEliminar);
         
         List<Usuario> usuarios = usuarioDao.listar();
         usuarios.forEach(usuario -> {
